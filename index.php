48033ba8654018f3bfee42c57776d8ece299a6dc
Mich <mich@example.com>
2019-01-15 17:24:32 +0100

    update php

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
+<?php
+	session_start();
 
-        
+	$_SESSION['connected'] = false;
+	$_SESSION['message'] = "";
+
+	include 'security.php';
+
+	$query =
+		'SELECT
+			id,
+			titre,
+			sous_titre,
+			corps,
+			date_publication
+		FROM article
+		INNER JOIN authors
+		ORDER BY date_creation DESC';
+
+	$result = $pdo -> query($query);
+	$posts = $result -> fetchAll();
+
+	$template = 'index';
+	include 'home.phtml';
